f5f345b1b1f010d2203f185b0895c8d9d72cfd8e
maestro27 <cjansenson@yahoo.com>
2017-02-11 15:50:32 -0500

    Simple caching example with Spring Boot

---
+++ caching/src/main/java/com/example/CachingApplication.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching  //Enables Spring's annotation-driven cache management capability
+public class CachingApplication implements CommandLineRunner{
+
+	@Autowired
+	private WeatherProvider weatherProvider;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CachingApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		System.out.println(weatherProvider.getWeather("30360"));
+		System.out.println(weatherProvider.getWeather("30360"));
+		System.out.println(weatherProvider.getWeather("30360"));
+	}
+}
---
+++ caching/src/main/java/com/example/WeatherProvider.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.stereotype.Component;
+import java.util.Random;
+
+import javax.cache.annotation.CacheResult;
+
+@Component
+public class WeatherProvider {
+	
+	@CacheResult
+	public String getWeather(String zipcode) {
+		int temp = 20 + new Random().nextInt(80);
+		int humidity = new Random().nextInt(100);
+		return String.format("Current temperature %1$d and humidity of %2$d", temp, humidity);
+	}
+
+}
